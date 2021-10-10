d9217c6b355919e751d17e29c9c927a3dfcb75ce
Dawid Szymkiewicz <dawid_szymkiewicz@hotmail.com>
2021-10-10 23:25:07 +0200

    Project initalization

---
+++ src/test/java/pl/com/januszex/paka/gui/PakaGuiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.januszex.paka.gui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PakaGuiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
